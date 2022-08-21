b2de519f05e8569d7daee279c94c9dc5b6880a99
RahulNeupane <nrahul503@gmail.com>
2022-08-21 19:19:39 +0545

    pipes added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const config = {
   height:600,
   physics: {
     //Arcade physics plugin manages physics simulation
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    }
   },
   scene:{
     preload,
@@ -15,30 +18,65 @@ const config = {
   }
 }
 
-
-//loading assets, such as image, music, animations ...
-function preload(){
-  //this context - scene
-  //contains funcions and properties we can use
-  this.load.image('sky','assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
+const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 
 let bird = null ;
-function create(){
-  // x - 400
-  // y - 300
-  // key of the image
-  this.add.image(0, 0, 'sky').setOrigin(0);
-  //middle of the height
-  bird = this.physics.add.sprite(config.width * 0.1, config.height/2 , 'bird').setOrigin(0)
-  bird.body.gravity.y = 200;
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
+
+const pipeOpeningDistanceRange = [150, 250];
+
+const flapVelocity = 250;
+const initialBirdPosition= {x: config.width * 0.1, y:config.height/2};
+
+
+function preload(){
+  this.load.image('sky','assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
-// 60fps
-// 60 times per second 
-// 60 * 8ms  = 480ms 
-function update(time,delta){
-  console.log(bird.body.velocity.y)
+
+function create(){
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y , 'bird').setOrigin(0);
+  bird.body.gravity.y = 400;
+  
+  for(let i = 0; i< PIPES_TO_RENDER; i++){
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeOpeningDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20,config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance,pipeVerticalPosition,'pipe').setOrigin(0,1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x,upperPipe.y +pipeVerticalDistance ,'pipe').setOrigin(0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
+
+
+  this.input.on('pointerdown',flap);
+  this.input.keyboard.on('keydown_SPACE',flap);
 }
+
+
+function update(time,delta){
+  if(bird.y > config.height || bird.y < -bird.height){
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition(){
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0; 
+}
+
+function flap(){
+  bird.body.velocity.y = -flapVelocity;
+}
+
 new Phaser.Game(config)
