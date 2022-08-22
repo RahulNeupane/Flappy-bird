3d1269c6af75019d6241e00bb71b80fbf2ccc4f3
RahulNeupane <nrahul503@gmail.com>
2022-08-22 18:50:48 +0545

    playscreen

--- src/index.js
+++ src/index.js
@@ -1,82 +1,26 @@
 import Phaser from "phaser";
+import PlayScene from "./scenes/PlayScene";
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT/2};
+
+const SHARED_CONFIG = {
+  width : WIDTH,
+  height : HEIGHT,
+  startPosition : BIRD_POSITION
+}
+
 const config = {
-  //WebGL (webgraphics library) Js API  for rendering 2D and 3D graphics
-  type: Phaser.AUTO,
-  width:800,
-  height:600,
+ type: Phaser.AUTO,
+  ...SHARED_CONFIG,
   physics: {
-    //Arcade physics plugin manages physics simulation
     default: 'arcade',
     arcade: {
       debug: true,
     }
   },
-  scene:{
-    preload,
-    create,
-    update,
-  }
-}
-
-const VELOCITY = 200;
-const PIPES_TO_RENDER = 4;
-
-let bird = null ;
-let upperPipe = null;
-let lowerPipe = null;
-let pipeHorizontalDistance = 0;
-
-
-const pipeOpeningDistanceRange = [150, 250];
-
-const flapVelocity = 250;
-const initialBirdPosition= {x: config.width * 0.1, y:config.height/2};
-
-
-function preload(){
-  this.load.image('sky','assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'assets/pipe.png');
-}
-
-
-function create(){
-  this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y , 'bird').setOrigin(0);
-  bird.body.gravity.y = 400;
-  
-  for(let i = 0; i< PIPES_TO_RENDER; i++){
-    pipeHorizontalDistance += 400;
-    let pipeVerticalDistance = Phaser.Math.Between(...pipeOpeningDistanceRange);
-    let pipeVerticalPosition = Phaser.Math.Between(0 + 20,config.height - 20 - pipeVerticalDistance);
-
-    upperPipe = this.physics.add.sprite(pipeHorizontalDistance,pipeVerticalPosition,'pipe').setOrigin(0,1);
-    lowerPipe = this.physics.add.sprite(upperPipe.x,upperPipe.y +pipeVerticalDistance ,'pipe').setOrigin(0);
-
-    upperPipe.body.velocity.x = -200;
-    lowerPipe.body.velocity.x = -200;
-  }
-
-
-  this.input.on('pointerdown',flap);
-  this.input.keyboard.on('keydown_SPACE',flap);
-}
-
-
-function update(time,delta){
-  if(bird.y > config.height || bird.y < -bird.height){
-    restartBirdPosition();
-  }
-}
-
-function restartBirdPosition(){
-  bird.x = initialBirdPosition.x;
-  bird.y = initialBirdPosition.y;
-  bird.body.velocity.y = 0; 
-}
-
-function flap(){
-  bird.body.velocity.y = -flapVelocity;
+  scene:[new PlayScene(SHARED_CONFIG)]
 }
 
 new Phaser.Game(config)
